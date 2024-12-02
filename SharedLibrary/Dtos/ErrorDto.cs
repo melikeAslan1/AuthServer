4cbea03d78d19db1a2a8769cbffb0d43acad3253
Melike Aslan <1melike2@gmail.com>
2024-12-02 23:34:44 +0300

    initiliaze project- katmanlar olusturuldu.

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; }  //sadece aşagidakilerle set edilir.
+
+        public bool IsShow { get; private set; } //true ise kullanıcıya gösterebilirsin.  false ise yazılımcının anlayacağı bir hatadır kullanıcıya göstermeye gerek yok.
+
+        public ErrorDto() {
+        
+            Errors = new List<String>();
+        
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+
+}
